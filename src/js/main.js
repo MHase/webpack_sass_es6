b74a5c00c03d13150e6e7d170394e2ff61ce7e7f
Maciej <maciek.sykula@gmail.com>
2017-05-24 11:59:35 +0200

    added html loader and npm jquery

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 // require('../css/style.scss');
 // import style from '../css/main.scss';
-
+/*jshint esversion: 6 */
+import 'jquery';
 let variable = 'Hello World!';
 
 console.log(variable);
+
+
+
+
+$(document).ready({
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: {
@@ -9,27 +10,44 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/[name].js',
-        publicPath: '/dist/'
+        // publicPath: '/dist'
     },
-    // devtool: "source-map",
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                },
-                exclude: /node_modules/
-            }, {
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
                 test: /\.scss$/,
                 loader: ExtractTextPlugin.extract({fallback: 'style-loader',
                   use: ['css-loader?url=false', 'resolve-url-loader', 'sass-loader']})
                   // use: ['css-loader?url=false', 'resolve-url-loader', 'sass-loader?sourceMap']})
             },
             {
-                test: /\.(png|jpg|gif)$/,
-                loader: "file-loader?name=img/[name].[ext]"
+              test: /\.html$/,
+              use: ['html-loader']
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
             }
         ]
     },
@@ -38,8 +56,14 @@ module.exports = {
         colors: true
     },
     plugins: [
-      new ExtractTextPlugin("css/[name].css")
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+      }),
+      new ExtractTextPlugin("css/[name].css"),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
     ],
     watch: false
-    // devtool: 'source-map'
 };
